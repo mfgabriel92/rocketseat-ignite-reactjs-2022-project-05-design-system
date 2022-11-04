8b53e83b59369ba16a9b1c52ceb6548fbedaacc6
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2022-11-04 14:17:26 +0800

    fix: add subpath to storybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  "viteFinal": (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/rocketseat-ignite-reactjs-2022-project-05-design-system";
+    }
+
+    return config;
   }
 };
